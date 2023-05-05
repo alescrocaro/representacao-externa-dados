f68d7c85d4b9a07b7513b36950394451dd61afb3
alescrocaro <alescrocaro@gmail.com>
2023-05-04 21:01:12 -0300

    esqueleto

--- atividade_red/server/db_connection.py
+++ atividade_red/server/db_connection.py
@@ -2,7 +2,7 @@ import os
 
 from pymongo import MongoClient
 from dotenv import load_dotenv
-from urllib.parse import quote_plus
+from urllib.parse import quote_plus # used to escape string
 
 load_dotenv()
 DB_USER = quote_plus(os.getenv('DB_USER'))
--- atividade_red/server/main.py
+++ atividade_red/server/main.py
@@ -2,23 +2,39 @@ from db_connection import MongoDBClient
 from pymongo.errors import PyMongoError
 
 
-# Create instance of MongoDBClient class
-mongo_client = MongoDBClient()
 
-client = mongo_client.get_client()
+class Server:
+    def __init__(self):
+        self.connected = False
+        # Create instance of MongoDBClient class
+        mongo_client = MongoDBClient()
 
-connected = False
+        self.db_client = mongo_client.get_client()
+        try:
+            # try to access a collection or run an operation to check connection
+            self.db = self.client['mydatabase']
+            self.collection = self.db['mycollection']
+            self.collection.count_documents({})
 
-try:
-    # try to access a collection or run an operation to check connection
-    db = client['mydatabase']
-    collection = db['mycollection']
-    collection.count_documents({})
+            print("MongoDB connection established!")
+            self.connected = True
 
-    print("MongoDB connection established!")
-    connected = True
+        except PyMongoError as e:
+            print("Connection failed:")
+            print(e)
 
-except PyMongoError as e:
-    print("Connection failed:")
-    print(e)
+    def CreateMovie(self):
+        if self.connected:
+            print('creating movie...')
 
+    def ReadMovie(self):
+        if self.connected:
+            print('reading movie...')
+
+    def UpdateMovie(self):
+        if self.connected:
+            print('updating movie...')
+
+    def DeleteMovie(self):
+        if self.connected:
+            print('deleting movie...')
